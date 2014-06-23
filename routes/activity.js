63f27a9e2b754015680a100205b37127daf87557
Benjamin Dean <benjamin@benjamindean.com>
2014-06-23 09:27:01 -0700

    Demonstrate in code how to use the update steps feature of configurations

--- routes/activity.js
+++ routes/activity.js
@@ -1,9 +1,49 @@
 'use strict';
 
-// NOTE: Each route can render a server-side view
 // Deps
+var util = require( 'util' );
 
 exports.logExecuteData = [];
+
+function logData( req ) {
+    exports.logExecuteData.push({
+        body: req.body,
+        headers: req.headers,
+        trailers: req.trailers,
+        method: req.method,
+        url: req.url,
+        params: req.params,
+        query: req.query,
+        route: req.route,
+        cookies: req.cookies,
+        ip: req.ip,
+        path: req.path,
+        host: req.host,
+        fresh: req.fresh,
+        stale: req.stale,
+        protocol: req.protocol,
+        secure: req.secure,
+        originalUrl: req.originalUrl
+    });
+        console.log( "body: " + util.inspect( req.body ) );
+        console.log( "headers: " + req.headers );
+        console.log( "trailers: " + req.trailers );
+        console.log( "method: " + req.method );
+        console.log( "url: " + req.url );
+        console.log( "params: " + util.inspect( req.params ) );
+        console.log( "query: " + util.inspect( req.query ) );
+        console.log( "route: " + req.route );
+        console.log( "cookies: " + req.cookies );
+        console.log( "ip: " + req.ip );
+        console.log( "path: " + req.path );
+        console.log( "host: " + req.host );
+        console.log( "fresh: " + req.fresh );
+        console.log( "stale: " + req.stale );
+        console.log( "protocol: " + req.protocol );
+        console.log( "secure: " + req.secure );
+        console.log( "originalUrl: " + req.originalUrl );
+}
+
 
 /*
  * POST Handler for / route of Activity (this is the edit route).
@@ -11,7 +51,7 @@ exports.logExecuteData = [];
 exports.edit = function( req, res ) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    exports.logExecuteData.push( req.body );
+    logData( req );
     res.send( 200, 'Edit' );
 };
 
@@ -21,7 +61,7 @@ exports.edit = function( req, res ) {
 exports.save = function( req, res ) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    exports.logExecuteData.push( req.body );
+    logData( req );
     res.send( 200, 'Save' );
 };
 
@@ -31,7 +71,7 @@ exports.save = function( req, res ) {
 exports.execute = function( req, res ) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    exports.logExecuteData.push( req.body );
+    logData( req );
     res.send( 200, 'Execute' );
 };
 
@@ -41,7 +81,7 @@ exports.execute = function( req, res ) {
 exports.publish = function( req, res ) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    exports.logExecuteData.push( req.body );
+    logData( req );
     res.send( 200, 'Publish' );
 };
 
@@ -51,6 +91,6 @@ exports.publish = function( req, res ) {
 exports.validate = function( req, res ) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-    exports.logExecuteData.push( req.body );
+    logData( req );
     res.send( 200, 'Validate' );
 };
